35ffea9affdaf040a93551b524443b5a6cc8d441
Breno Santos <brenovinicius.santos0@gmail.com>
2024-12-11 21:00:59 -0300

    function as parameter of functions - Predicate

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import entities.services.ProductService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.fiteredSum(list, p -> p.getName().charAt(0) == 'T');
+
+		System.out.println("Sum: " + sum);
+	}
+
+}
---
+++ src/entities/services/ProductService.java
@@ -0,0 +1,18 @@
+package entities.services;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductService {
+
+	public double fiteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0;
+		for (Product p : list) {
+			if (criteria.test(p))
+			sum += p.getPrice();
+		}
+		return sum;
+	}
+}
